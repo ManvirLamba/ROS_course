4e85618d8839ccdf3d6cf26e75b7fc5af9f7ab36
Manvir Singh <lambamanvir@gmail.com>
2021-07-20 03:02:19 +0530

    whereami

---
+++ whereAmI/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/manvir/udacityCoursework/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/manvir/udacityCoursework/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/manvir/udacityCoursework/catkin_ws/devel;/opt/ros/melodic' != '' else []
