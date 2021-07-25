eb21c77de8f5d8d362760458f067cd42fadd18f9
Manvir Singh <lambamanvir@gmail.com>
2021-07-25 15:46:58 +0530

    initial commit

---
+++ MapMyWorld/build/pgm_map_creator/msgs/CMakeFiles/collision_map_creator_msgs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/manvir/udacityCoursework/catkin_ws/devel/lib/libcollision_map_creator_msgs.pdb"
+  "/home/manvir/udacityCoursework/catkin_ws/devel/lib/libcollision_map_creator_msgs.so"
+  "CMakeFiles/collision_map_creator_msgs.dir/collision_map_request.pb.cc.o"
+  "CMakeFiles/collision_map_creator_msgs.dir/collision_map_request.pb.cc.o.d"
+  "collision_map_request.pb.cc"
+  "collision_map_request.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/collision_map_creator_msgs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
